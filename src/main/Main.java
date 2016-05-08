1e2e01b8262b3d2228da35eaa6c5bc0d7d7a8799
y0n1 <y0n1@outlok.com>
2016-05-08 16:20:14 +0300

    Moving client code test to JUnit.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,8 @@
 package main;
 
-import dictionary.Dictionary;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Dictionary dic = Dictionary.getInstance();
-		dic.addWord("bear");
-		dic.addWord("beer");
-		dic.addWord("beans");
-		dic.addWord("bean");
-		dic.addWord("be");
-		dic.printAllWords("b");
-		
-		dic.removeWord("be");
-		dic.printAllWords("b");
-		
-		dic.printAllWords("a");
-		
+				
 	}
 }
